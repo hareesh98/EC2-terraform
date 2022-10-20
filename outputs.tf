695177ce658110e967c2efd3f34afe67d94d3d1a
hareesh98 <95620791+hareesh98@users.noreply.github.com>
2022-10-20 16:38:54 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.test_server.id
+  value       = aws_instance.MYJENKINSSERVER.id
 }
 
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.test_server.public_ip
+  value       = aws_instance.MYJENKINSSERVER.public_ip
 }
