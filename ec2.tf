ce8a0f6761990d30adeed8bbc0f6393816d82256
hareesh98 <95620791+hareesh98@users.noreply.github.com>
2022-10-20 16:15:59 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -18,3 +18,65 @@ resource "aws_instance" "test_server" {
   }
 }
 
+variable "access_key" {
+  description = "aws access key"
+  type        = string
+}
+
+variable "secret_access_key" {
+  description = "aws secret access key"
+  type        = string
+}
+
+variable "region" {
+  description = "aws region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "instance_type" {
+  description = "instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami" {
+  description = "instance ami"
+  type = string
+}
+
+variable "associate_public_ip_address" {
+  description = "assigning public ip address"
+  type = bool
+}
+
+variable "monitoring" {
+  description =  "enabling monitoring"
+  type = bool
+  default = "false"
+}
+
+variable "vpc_security_group_ids" {
+  description =  "VPC Security groups to attach to ec2 instance"
+  type = list
+}
+
+variable "subnet_id" {
+  description = "subnet id"
+  type = string
+}
+
+variable "tags" {
+  description =  "ec2 instance tags"
+  type = map
+}
+
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.test_server.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.test_server.public_ip
+}
