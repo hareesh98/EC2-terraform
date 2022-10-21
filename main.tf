1ef15d3a180cc5eaf99f989242010649de980c37
hareesh98 <95620791+hareesh98@users.noreply.github.com>
2022-10-21 15:27:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "MYJENKINSSERVER" {
   associate_public_ip_address = var.associate_public_ip_address
   monitoring = var.monitoring
   vpc_security_group_ids = var.vpc_security_group_ids
-  count = 3
+  count = 2
   subnet_id = var.subnet_id
   tags = var.tags
   timeouts {
