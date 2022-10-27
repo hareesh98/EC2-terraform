623d04d19924cfb53faa907a16e383bec5d4782f
hareesh98 <95620791+hareesh98@users.noreply.github.com>
2022-10-27 12:01:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "originalinstance" {
+resource "aws_instance" "MYJENKINSSERVER" {
   ami           = var.ami 
   instance_type = var.instance_type
   associate_public_ip_address = var.associate_public_ip_address
