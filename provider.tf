135882b6a014ef7f4eb6bb837a79899e97ea40ff
hareesh98 <95620791+hareesh98@users.noreply.github.com>
2022-11-10 11:53:00 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,10 +6,11 @@ terraform {
     }
   }
 
-  required_version = ">= 1.0.0"
+  required_version = ">= 0.14.9"
 }
+
 provider "aws" {
-  region     = var.region
   access_key = var.access_key
-  secret_access_key = var.secret_access_key
-  }
+  secret_key = var.secret_access_key
+  region  = var.region 
+}
