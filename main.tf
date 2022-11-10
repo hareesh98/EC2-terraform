c37f9fb65421821bdcb9038acba231ff23530b02
hareesh98 <95620791+hareesh98@users.noreply.github.com>
2022-11-10 13:41:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "myinstance" {
+resource "aws_instance" "A" {
   ami           = var.ami 
   instance_type = var.instance_type
   associate_public_ip_address = var.associate_public_ip_address
