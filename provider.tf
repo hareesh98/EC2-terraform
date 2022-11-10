f5faa5b2dbe3971ba573cbce62f50ec6649b4068
hareesh98 <95620791+hareesh98@users.noreply.github.com>
2022-11-10 11:56:41 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "aws"
-      version = "~> 3.27"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-}
-
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_access_key
