92316585aac7f54be01d0275c3930ef8689abca9
hareesh98 <95620791+hareesh98@users.noreply.github.com>
2022-11-23 16:25:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "DEMO-T" {
   ami           = var.ami 
   instance_type = var.instance_type
   associate_public_ip_address = var.associate_public_ip_address
+  subnet_id = var.subnet_id
   monitoring = var.monitoring
   vpc_security_group_ids = var.vpc_security_group_ids
   tags = var.tags
