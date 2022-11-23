bf7738e7296e1e8f15a7a34029282eb783434863
hareesh98 <95620791+hareesh98@users.noreply.github.com>
2022-11-23 16:09:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "DEMO-TEST" {
+resource "aws_instance" "DEMO-T" {
   ami           = var.ami 
   instance_type = var.instance_type
   associate_public_ip_address = var.associate_public_ip_address
